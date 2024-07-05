aca43789aa54512693d6ec87e1ff96eb066ab04a
tootydooty <vlaadiolus@gmail.com>
2024-07-06 01:34:42 +0300

    Init lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
-pub mod app;
+#![feature(type_alias_impl_trait)]
+
+pub mod manager;
+pub mod mediator;
+pub mod routines;
+
+pub mod prelude {
+    pub use crate::manager::*;
+    pub use crate::routines::*;
+}
