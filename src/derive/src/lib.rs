ca0ffc63fb107bbd187166e7a24a303ae8f801b9
Give me an Oscar <vlaadiolus@gmail.com>
2024-07-25 02:06:36 +0300

    Add 'app_route' and 'app_event' macros

---
+++ src/derive/src/lib.rs
@@ -0,0 +1,27 @@
+extern crate proc_macro;
+extern crate proc_macro2;
+#[macro_use]
+extern crate quote;
+extern crate syn;
+
+use proc_macro2::TokenStream;
+
+#[proc_macro_attribute]
+pub fn app_route(_metadata: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(std::fmt::Debug, Clone, PartialEq, Eq, std::hash::Hash)]
+        #input
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn app_event(_metadata: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(Clone, PartialEq)]
+        #input
+    };
+    output.into()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,11 @@ pub mod manager;
 pub mod mediator;
 pub mod routines;
 
+pub use derive;
+
+
 pub mod prelude {
     pub use crate::manager::*;
     pub use crate::routines::*;
+    pub use crate::derive::*;
 }
